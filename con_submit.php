96ffc3fc417021e9c7241361009747c937f72cbb
pmy0721 <85235079+pmy0721@users.noreply.github.com>
2021-07-11 22:29:03 +0800

    Add files via upload

---
+++ con_submit.php
@@ -0,0 +1,25 @@
+<?php
+	header("content-type:text/html;charset=utf-8");
+	//$con = mysql_connect("IP","name","password");
+	$conn=mysqli_connect("localhost","root","123456","test1");
+
+	if (!$conn)
+  	{
+  	die('Could not connect: ' . mysqli_error($conn));
+  	}
+	//mysql_select_db("my_db", $con);
+
+	//设置客户端和连接字符集
+	mysqli_query($conn,"set names utf8");
+
+	$sql="INSERT INTO contactme_db (name, message) VALUES('$_POST[name]','$_POST[message]')";
+	//if (!mysql_query($sql,$con))
+	if (!mysqli_query($conn,$sql))
+  	{
+  	die('Error: ' . mysqli_error($conn));
+  	}
+	//echo "您的学校是：".$_POST["school"]."<br/>";
+	//echo "您的专业是：".$_POST["major"]."<br/>";
+	//echo "您的姓名是：".$_POST["name"]."<br/>";
+	mysqli_close($conn);
+?>
